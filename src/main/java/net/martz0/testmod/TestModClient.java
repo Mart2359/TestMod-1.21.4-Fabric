d1bb9447c25821200c13db643b28d307fef2ed7d
Mart2359 <[you@example.com])>
2025-03-06 21:13:02 +0000

    Initial commit

---
+++ src/main/java/net/martz0/testmod/TestModClient.java
@@ -0,0 +1,10 @@
+package net.martz0.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
