b207321df9cb2030522cb7c39eba38e4d280ed17
Mart2359 <[you@example.com])>
2025-03-08 16:24:04 +0000

    Added Diamond Transmuter

--- src/main/java/net/martz0/testmod/item/ModItems.java
+++ src/main/java/net/martz0/testmod/item/ModItems.java
@@ -1,6 +1,8 @@
 package net.martz0.testmod.item;
 
 import net.martz0.testmod.TestMod;
+import net.martz0.testmod.item.custom.TransmuterItem;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryKey;
@@ -14,6 +16,8 @@ public final class ModItems {
     public static final Item DARK_FEATHER = register("dark_feather", Item::new, new Item.Settings());
     public static final Item RAMA_ESSENCE = register("rama_essence", Item::new, new Item.Settings());
     public static final Item RAMA_CRYSTAL = register("rama_crystal", Item::new, new Item.Settings());
+
+    public static final Item DIAMOND_TRANSMUTER = register("diamond_transmuter", settings -> new TransmuterItem(Blocks.DIAMOND_BLOCK, settings), new Item.Settings().maxDamage(64));
 
     public static Item register(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
         final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TestMod.MOD_ID, path));
---
+++ src/main/java/net/martz0/testmod/item/custom/TransmuterItem.java
@@ -0,0 +1,47 @@
+package net.martz0.testmod.item.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class TransmuterItem extends Item {
+
+    private final Block transmuter_block;
+
+    public TransmuterItem(Block transmuter_block, Settings settings) {
+        super(settings);
+        this.transmuter_block = transmuter_block;
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        PlayerEntity playerEntity = context.getPlayer();
+        World world = context.getWorld();
+        BlockPos blockPos = context.getBlockPos();
+        BlockState blockState = world.getBlockState(blockPos);
+
+        if (blockState.isSolidBlock(world, blockPos) && blockState.isFullCube(world, blockPos) && !blockState.hasBlockEntity()) {
+            if (!world.isClient()) {
+                world.playSound(playerEntity, blockPos, SoundEvents.BLOCK_GRINDSTONE_USE, SoundCategory.BLOCKS, 1.0F, world.getRandom().nextFloat() * 0.4F + 0.8F);
+                world.setBlockState(blockPos, transmuter_block.getDefaultState());
+
+                if (playerEntity instanceof ServerPlayerEntity) {
+                    context.getStack().damage(1, playerEntity, LivingEntity.getSlotForHand(context.getHand()));
+                }
+            }
+            return ActionResult.SUCCESS;
+        } else {
+            return ActionResult.FAIL;
+        }
+    }
+}
